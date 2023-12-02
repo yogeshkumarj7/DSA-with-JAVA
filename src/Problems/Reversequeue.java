914d742e3a77c7cacad0ca8076d8781483cec8a6
Yogesh <coolcoder9334@gmail.com>
2023-12-02 23:57:31 +0530

    Revised Queue

---
+++ src/Problems/Reversequeue.java
@@ -0,0 +1,32 @@
+package Problems;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+public class Reversequeue {
+    public static void reverse(Queue<Integer> q){
+        Stack<Integer> s=new Stack<>();
+        while(!q.isEmpty()){
+            s.push(q.remove());
+        }
+        while (!s.isEmpty()){
+            q.add(s.pop());
+        }
+    }
+    public static void main(String[] args) {
+        Queue<Integer>q=new LinkedList<>();
+        q.add(10);
+        q.add(20);
+        q.add(30);
+        q.add(40);
+        q.add(50);
+        reverse(q);
+
+//        Printing the queue
+
+        while (!q.isEmpty()){
+            System.out.println(q.remove());
+        }
+        System.out.println();
+    }
+}
